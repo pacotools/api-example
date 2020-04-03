df4407a9f9b61283a879447270016dc2b7017ea4
Juan Paco <juanfjuarez@kolibee.com>
2020-04-03 02:25:17 +0000

    github

---
+++ assets/js/github-information.js
@@ -0,0 +1,16 @@
+function fetchGitHubInformation(event) {
+    var username = $(#gh-username).val();
+
+    if (!username) {
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
+        return;
+    }
+
+    // display a loader that is an animated gif file that will just keep
+    // repeating itself while data has been accessed.
+    
+    $("#gh-user-data").html(
+        `<div id="loader">
+            <img scr="assets/css/loader.gif" alt="loading..." />`
+    )
+}
